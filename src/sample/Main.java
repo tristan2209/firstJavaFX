047fa4c8314f4e7a0e2f3ab3fef326c0c669f5fd
Briziou_Baptiste <baptiste.briziou@etu.univ-poitiers.fr>
2021-03-20 14:20:25 +0100

    Salut

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.swing.*;
-import java.awt.*;
+
 
 public class Main extends Application {
     private JLabel label;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent test = FXMLLoader.load(getClass().getResource("Test.fxml"));
+        primaryStage.setTitle("Liste des étudiants de Gphy");
+
+        primaryStage.setScene(new Scene(test, 300, 275));
+
         primaryStage.show();
     }
 
