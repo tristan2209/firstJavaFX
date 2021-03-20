74116ff93b6443a67dc59eaad1cfec5eae7c45cb
silvestre_ewan <ewan.silvestre@etu.univ-poitiers.fr>
2021-03-20 15:11:06 +0100

    Affichage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,14 +18,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Parent test = FXMLLoader.load(getClass().getResource("Test.fxml"));
-        Parent test = FXMLLoader.load(getClass().getResource("ajout.fxml"));
+        Parent Acceuil = FXMLLoader.load(getClass().getResource("Test.fxml"));
+        Parent Ajout = FXMLLoader.load(getClass().getResource("ajout.fxml"));
+        Parent Affichage = FXMLLoader.load(getClass().getResource("Affichage.fxml"));
         primaryStage.setTitle("Liste des étudiants de Gphy");
 
-
-
-        primaryStage.setScene(new Scene(test));
+        primaryStage.setScene(new Scene(Acceuil, 300, 275));
+        primaryStage.setScene(new Scene(Ajout, 300, 275));
+        primaryStage.setScene(new Scene(Affichage, 300, 275));
 
         primaryStage.show();
     }
