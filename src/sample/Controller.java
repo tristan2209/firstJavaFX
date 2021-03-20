7986c36cffc9576171fc325cfed2a5021bd35050
Briziou_Baptiste <baptiste.briziou@etu.univ-poitiers.fr>
2021-03-20 15:56:55 +0100

    Les plus beaux

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,27 @@
 package sample;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
 
 public class Controller {
+    @FXML
+    private Menu Ajout;
+
+    @FXML
+    private Menu Affichage;
+
+    @FXML
+    void AffichagePressed(ActionEvent event) {
+        System.out.println("good");
+
+    }
+
+    @FXML
+    void AjoutPressed(ActionEvent event) {
+        System.out.println("bad");
+    }
+
+
+
 
 }
